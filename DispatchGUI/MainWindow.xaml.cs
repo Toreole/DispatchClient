1a3cd0bf9c0c7483504797303ba961b2d8aa3d2b
Toreole <toreole.official@gmail.com>
2020-05-03 22:05:27 +0200

    I managed to get a Button to work, important save

--- DispatchGUI/MainWindow.xaml.cs
+++ DispatchGUI/MainWindow.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace DispatchGUI
 {
     public class MainWindow : Window
     {
+        private Button testButton;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +20,15 @@ namespace DispatchGUI
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            testButton = this.FindControl<Button>("TestButton");
+            testButton.IsEnabled = true;
+            testButton.Click += OnClickTest;
+        }
+
+        public void OnClickTest(object sender, RoutedEventArgs args)
+        {
+            testButton.Content = "yikes";
+            testButton.IsEnabled = false;
         }
     }
 }
