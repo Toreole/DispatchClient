5c2dcffa9fbd289e1e8a18cfff97caf12bd7b1b3
Toreole <toreole.official@gmail.com>
2020-05-06 15:08:37 +0200

    installer in the clear for now

--- DispatchGUI/App.xaml.cs
+++ DispatchGUI/App.xaml.cs
@@ -17,7 +17,24 @@ namespace DispatchGUI
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                //TODO: this todo database stems from the avalonia tutorial and all connected resources can be removed.
                 var db = new Services.TodoDatabase();
+                //filter the command line arguments
+                var commandArguments = System.Environment.GetCommandLineArgs();
+                if(commandArguments.Length > 0)
+                {
+                    //there is an argument, currently that should only be the case if it gets passed a path!
+                    string path = commandArguments[0];
+                    if(path.EndsWith(".disgui"))
+                    {
+                        //identified as file
+                    }
+                    else
+                    {
+                        //is a directory.
+                    }
+                }
+                //make the main window.
                 desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(db),
--- InstallerWindows/Program.cs
+++ InstallerWindows/Program.cs
@@ -14,47 +14,23 @@ namespace InstallerWindows
         static void Main()
         {
             Program program = new Program();
-            //program.Run();
-            program.TestRegistry();
+            program.Install();
+            //TODO: option to uninstall.
 
             Console.WriteLine("Setup will exit now.");
             Console.ReadKey();
         }
         //"PLACEHOLDER_URL/RELEASE_FILE"
-        readonly Uri downloadUri = new Uri("https://google.com/");
-        readonly string tempFile = "TEMP_FILE";
+        readonly Uri downloadUri = new Uri("https://github.com/Toreole/DispatchClient/releases/latest/app.zip");
+        readonly string tempFile = "TEMP_DOWNLOAD.zip";
 
         ProgressBar progressBar;
         WebClient client;
         string installationPath;
         string tempFilePath;
+        string exePath;
 
-        public void TestRegistry()
-        {
-            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software").OpenSubKey("Classes").OpenSubKey("Directory").OpenSubKey("shell");
-            //Set the icon: \shell\DispatchGUI "Icon" path_to_exe
-            //Text:         \shell\DispatchGUI "" Create DispatchGUI here
-            //Command:      \shell\DispatchGUI\command "" <path.exe> %1         //the %1 is the context path.
-
-            //Add the Entry for the context menu on background
-            //Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI", "", "Open DispatchGUI here");
-            //Set the icon
-            //Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI", "Icon", "\"<pathToExe>\", 1");
-            //Define the command to run
-            //Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI\command", "", "\"<pathToExe>\" \"%1\"");
-
-            //Define the filetype:
-            //SetValue HKEY_CLASSES_ROOT\.disgui disguiproj
-            //SetValue HKEY_CLASSES_ROOT\disguiproj Name:"" Value:"DispatchGUI Project"
-
-            //Default Icon:
-            //SetValue HKEY_CLASSES_ROOT\<FileTypeName>\DefaultIcon Name:"" Value:"\"<pathToExe>\", 1"
-
-            //Set the application to open the file
-            //SetValue HKEY_CLASSES_ROOT\<FileTypeName>\shell\open\command Name:"" Value:"\"<pathToExe>\" \"%1\""
-        }
-
-        public void Run()
+        public void Install()
         {
             FolderBrowserDialog fileDialog = new FolderBrowserDialog();
 
@@ -90,12 +66,35 @@ namespace InstallerWindows
         //The download is complete, go ahead with installing it.
         void OnDownloadComplete(object sender, AsyncCompletedEventArgs e)
         {
+            this.client.Dispose();
             //Unzip the downloaded release into the installation path.
             ZipFile.ExtractToDirectory(this.tempFilePath, this.installationPath);
-
-            //Setup the registry stuff: context menu.
+            this.exePath = Path.Combine(installationPath, "DispatchGUI.exe");
             
+            //Setup the registry stuff: context menu.
+            //Add the Entry for the context menu on background
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI", "", "Open DispatchGUI here");
+            //Set the icon
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI", "Icon", $"\"{this.exePath}\", 1");
+            //Define the command to run
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Classes\Directory\Background\shell\DispatchGUI\command", "", $"\"{this.exePath}\" \"%1\"");
+
             //Setup the .disgui file type and the connection with the installed program.
+            //Define the filetype:
+            //SetValue HKEY_CLASSES_ROOT\.disgui disguiproj
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\.disgui", "", "disguiproj");
+            //SetValue HKEY_CLASSES_ROOT\disguiproj Name:"" Value:"DispatchGUI Project"
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\disguiproj", "", "DispatchGUI Project");
+
+            //Default Icon:
+            //SetValue HKEY_CLASSES_ROOT\<FileTypeName>\DefaultIcon Name:"" Value:"\"<pathToExe>\", 1"
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\disguiproj\DefaultIcon", valueName: "", value: $"\"{this.exePath}\", 1");
+
+            //Set the application to open the file
+            //SetValue HKEY_CLASSES_ROOT\<FileTypeName>\shell\open\command Name:"" Value:"\"<pathToExe>\" \"%1\""
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\disguiproj\shell\open\command", valueName: "", value: $"\"{this.exePath}\", \"%1\"");
+
+            progressBar.Dispose();
         }
     }
 }
